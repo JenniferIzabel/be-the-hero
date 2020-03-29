d595c55cb31b0abcb839a9dcfff2657ac7aee160
Jennifer <izabeljennifer@gmail.com>
2020-03-29 11:00:25 -0300

    front-end

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -15,16 +15,28 @@ export default function Register() {
     const [city, setCity] = useState('');
     const [uf, setUf] = useState('');
 
-    function handleRegister(e) {
+    const history = useHistory();
+
+    async function handleRegister(e) {
         e.preventDefault();
 
-        console.log({
+        const data = {
             name,
             email,
             whatsapp,
             city,
             uf
-        })
+        };
+
+       try {
+        const response = await api.post('ongs', data);
+
+        alert(`Seu ID de acesso: ${response.data.id}`);
+
+        history.push('/');
+       } catch (err) {
+           alert('Erro no cadastro, tente novamente.');
+       }
     }
 
     return(
@@ -46,30 +58,30 @@ export default function Register() {
                    <input 
                         placeholder="Nome da ONG"
                         value={name}
-                        oncChange={e => setName(e.target.value)}
+                        onChange={e => setName(e.target.value)}
                     />
                    <input 
                         type="email" 
                         placeholder="E-mail" value={email}
-                        oncChange={e => setEmail(e.target.value)}
+                        onChange={e => setEmail(e.target.value)}
                     />
                    <input 
                         placeholder="WhatsApp"
                         value={whatsapp}
-                        oncChange={e => setWhatsapp(e.target.value)}
+                        onChange={e => setWhatsapp(e.target.value)}
                     />
 
                    <div className="input-group">
                        <input 
                             placeholder="Cidade" 
                             value={city}
-                            oncChange={e => setCity(e.target.value)}
+                            onChange={e => setCity(e.target.value)}
                         />
                        <input 
                             placeholder="UF" 
                             width="80"
                             value={uf}
-                            oncChange={e => setUf(e.target.value)}
+                            onChange={e => setUf(e.target.value)}
                         />
                    </div> 
 
